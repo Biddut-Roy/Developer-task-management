186c494181611df9fd2dea8b06652a8bf1c2fd8f
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-12-20 23:42:05 +0600

    errorpage component complete

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../Component/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <div>Hello world!</div>,
-        errorElement: <div>Hello Error page</div>,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/contacts",
