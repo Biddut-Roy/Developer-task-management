f3f0fd00ba78751c88c1f2ffe80a1abd6ef05105
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-12-22 10:37:50 +0600

    all task component AOs animation setup

--- src/Layout/Dashboard/Component/AllTask.jsx
+++ src/Layout/Dashboard/Component/AllTask.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import useAuth from "../../../Hooks/useAuth";
 import usePublicAxios from "../../../Hooks/usePublicAxios";
 import { format } from "date-fns";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const AllTask = () => {
     const { user } = useAuth();
@@ -14,13 +15,18 @@ const AllTask = () => {
             .then(res => setTasks(res?.data))
     }, [user?.email])
 
+    AOS.init({
+        easing: 'ease-out-quart',
+        delay: 0,
+        duration: 750
+        })
 
 
     return (
         <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2 md:gap-5 lg:gap-10 w-11/12 mx-auto mt-10">
             {
             tasks?.map((task) => (
-              <div key={task._id} className="card bg-base-100 shadow-xl ">
+              <div key={task._id} data-aos="fade-left" className="card bg-base-100 shadow-xl ">
                 <div className="card-body bg-slate-300 text-black">
                 <h2 className="card-title">Title: {task.Title}</h2>
                     <p>{task.Descriptions}</p>
