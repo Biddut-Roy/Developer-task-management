22eeffcca2df96a0e32292b8b9657ea6b4bea4b8
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-12-22 10:34:21 +0600

    Aos animation setup complete

--- src/Page/Gallary/Gallery.jsx
+++ src/Page/Gallary/Gallery.jsx
@@ -1,20 +1,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Gallery = () => {
     const [data, setData] = useState([]);
+
+    AOS.init({
+        easing: 'ease-out-quart',
+        delay: 0,
+        duration: 750
+        })
 
     useEffect(() => {
         axios("gallery.json")
             .then(res => setData(res.data))
     }, [])
-    console.log(data);
+
     return (
         <div className=" bg-slate-50 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2 md:gap-5 lg:gap-8 py-12 ">
            {
             data?.map(item => (
-                <div key={item.id} className="card bg-white shadow-xl">
+                <div key={item.id}  data-aos="flip-up" className="card bg-white shadow-xl">
                 <figure><img src={item.url} alt="img" /></figure>
             </div>
             ))
